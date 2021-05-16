14fc1149debfefea3021d221b8a21c6569817284
RJCF <rodrigojcf19@gmail.com>
2021-05-16 11:18:46 -0300

    atualização

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const lerAgora = document.querySelector('#ler-Agora');
+const informacoes = document.querySelector('#informacoes');
+
+lerAgora.addEventListener('click', menu);
+informacoes.addEventListener('click', info);
+
+function menu() {
+  window.location.href = 'tecnomancer.html';
+}
+
+function info() {
+  window.location.href = 'tecnomancer.html';
+}
